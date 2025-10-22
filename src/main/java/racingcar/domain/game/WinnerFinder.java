2eeb91d928b75fd79f41ed579987e38c4d53cfb1
softmoca <acyc0909@gmail.com>
2025-10-22 17:11:33 +0900

    feat : 최대 위치를 가진 자동차들 필터링 기능 구

--- src/main/java/racingcar/domain/game/WinnerFinder.java
+++ src/main/java/racingcar/domain/game/WinnerFinder.java
@@ -9,7 +9,7 @@ public class WinnerFinder {
 
     public List<Car> findWinners(List<Car> cars) {
         Position maxPosition = findMaxPosition(cars);
-        return ;
+        return filterWinners(cars, maxPosition);
     }
 
     private Position findMaxPosition(List<Car> cars) {
@@ -21,6 +21,16 @@ public class WinnerFinder {
             }
         }
         return maxPosition;
+    }
+
+    private List<Car> filterWinners(List<Car> cars, Position maxPosition) {
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition().equals(maxPosition)) {
+                winners.add(car);
+            }
+        }
+        return winners;
     }
 
 
