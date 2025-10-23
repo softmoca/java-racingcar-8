b5bb259f0d2791f3118f01dc0b53e5c9f82a3c91
softmoca <acyc0909@gmail.com>
2025-10-23 21:02:13 +0900

    test : WinnerFinder 테스트 추가
    - 단독 우승자 및 공동 우승자(2명, 3명) 판별 로직 테스트
    - 각 케이스별 Car 이동 상태를 통해 결과 테스트
    - WinnerFinder의 우승자 판정 정확성 보장 테스트

---
+++ src/test/java/racingcar/domain/game/WinnerFinderTest.java
@@ -0,0 +1,132 @@
+package racingcar.domain.game;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.car.Car;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("WinnerFinder 클래스 테스트")
+class WinnerFinderTest {
+
+    private WinnerFinder winnerFinder= new WinnerFinder();;
+
+    @Test
+    @DisplayName("단독 우승자를 찾는다")
+    void findSingleWinner() {
+        // given
+        Car car1 = new Car("moca");
+        Car car2 = new Car("coco");
+        Car car3 = new Car("aa");
+
+        car1.move(true);
+        car1.move(true);
+        car1.move(true);
+
+        car2.move(true);
+        car2.move(true);
+
+        car3.move(true);
+
+        List<Car> cars = Arrays.asList(car1, car2, car3);
+
+        // when
+        List<Car> winners = winnerFinder.findWinners(cars);
+
+        // then
+        assertThat(winners).hasSize(1);
+        assertThat(winners.get(0).getName()).isEqualTo("pobi");
+    }
+
+    @Test
+    @DisplayName("공동 우승자를 찾는다 - 2명")
+    void findMultipleWinners() {
+        // given
+        Car car1 = new Car("moca");
+        Car car2 = new Car("coco");
+        Car car3 = new Car("aaa");
+
+        car1.move(true);
+        car1.move(true);
+        car1.move(true);
+
+        car2.move(true);
+        car2.move(true);
+        car2.move(true);
+
+        car3.move(true);
+
+        List<Car> cars = Arrays.asList(car1, car2, car3);
+
+        // when
+        List<Car> winners = winnerFinder.findWinners(cars);
+
+        // then
+        assertThat(winners).hasSize(2);
+        assertThat(winners)
+                .extracting(Car::getName)
+                .containsExactlyInAnyOrder("moca", "coco");
+    }
+
+    @Test
+    @DisplayName("공동 우승자를 찾는다 - 3명")
+    void findAllWinnersWhenTied() {
+        // given
+        Car car1 = new Car("moca");
+        Car car2 = new Car("coco");
+        Car car3 = new Car("aa");
+        Car car4 = new Car("bb");
+
+        car1.move(true);
+        car1.move(true); // position: 2
+
+        car2.move(true);
+        car2.move(true); // position: 2
+
+        car3.move(true);
+        car3.move(true); // position: 2
+
+        car4.move(true); // position: 1
+
+        List<Car> cars = Arrays.asList(car1, car2, car3, car4);
+
+        // when
+        List<Car> winners = winnerFinder.findWinners(cars);
+
+        // then
+        assertThat(winners).hasSize(3);
+        assertThat(winners)
+                .extracting(Car::getName)
+                .containsExactlyInAnyOrder("moca", "coco", "aa");
+    }
+
+    @Test
+    @DisplayName("모든 자동차가 같은 위치면 모두 우승자다")
+    void findAllWinnersWhenAllTied() {
+        // given
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("woni");
+        Car car3 = new Car("jun");
+
+        car1.move(true);
+        car2.move(true);
+        car3.move(true);
+
+        List<Car> cars = Arrays.asList(car1, car2, car3);
+
+        // when
+        List<Car> winners = winnerFinder.findWinners(cars);
+
+        // then
+        assertThat(winners).hasSize(3);
+        assertThat(winners)
+                .extracting(Car::getName)
+                .containsExactlyInAnyOrder("pobi", "woni", "jun");
+    }
+
+
+}
