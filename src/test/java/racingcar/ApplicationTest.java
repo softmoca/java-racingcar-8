9ed4f5698197a9ff6e5c6cee65efb136e381d908
softmoca <acyc0909@gmail.com>
2025-10-27 16:04:26 +0900

    test : 시도 횟수 예외 상황 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -18,6 +18,9 @@ class ApplicationTest extends NsTest {
     private static final String CONSECUTIVE_DELIMITER_ERROR = "연속된 쉼표는 허용되지 않습니다.";
     private static final String START_WITH_DELIMITER_ERROR = "쉼표로 시작할 수 없습니다.";
     private static final String END_WITH_DELIMITER_ERROR = "쉼표로 끝날 수 없습니다.";
+    private static final String NOT_NUMBER_ERROR = "시도 횟수는 숫자여야 합니다.";
+    private static final String NOT_POSITIVE_ERROR = "시도 횟수는 1 이상이어야 합니다.";
+
 
     @Test
     void 기능_테스트() {
@@ -88,7 +91,48 @@ class ApplicationTest extends NsTest {
         );
     }
 
-    
+
+    @Test
+    @DisplayName("시도 횟수에 대한 예외 처리 - 빈 값인 경우")
+    void shouldThrowExceptionWhenAttemptCountIsBlank() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("mo,ca", " "))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(NOT_NUMBER_ERROR)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수에 대한 예외 처리 - 숫자가 아닌 경우 (One)")
+    void shouldThrowExceptionWhenAttemptCountIsNotNumber1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("mo,ca", "One"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(NOT_NUMBER_ERROR)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수에 대한 예외 처리 - 숫자가 아닌 경우 (1a2)")
+    void shouldThrowExceptionWhenAttemptCountIsNotNumber2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("mo,caa", "1a2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(NOT_NUMBER_ERROR)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수에 대한 예외 처리 - 음수인 경우")
+    void shouldThrowExceptionWhenAttemptCountIsNegative() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("moca,coco", "-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(NOT_POSITIVE_ERROR)
+        );
+    }
+
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
