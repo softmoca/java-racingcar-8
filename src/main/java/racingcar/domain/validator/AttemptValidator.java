0982b45495560dae897bc124f4e9da92f1552dd6
softmoca <acyc0909@gmail.com>
2025-10-27 14:19:03 +0900

    AttemptValidator의 에러 메시지 pravite에서 디폴트로 접근제어자 수정

--- src/main/java/racingcar/domain/validator/AttemptValidator.java
+++ src/main/java/racingcar/domain/validator/AttemptValidator.java
@@ -1,8 +1,8 @@
 package racingcar.domain.validator;
 
 public class AttemptValidator {
-    private static final String NOT_NUMBER_ERROR = "시도 횟수는 숫자여야 합니다.";
-    private static final String NOT_POSITIVE_ERROR = "시도 횟수는 1 이상이어야 합니다.";
+    static final String NOT_NUMBER_ERROR = "시도 횟수는 숫자여야 합니다.";
+    static final String NOT_POSITIVE_ERROR = "시도 횟수는 1 이상이어야 합니다.";
 
     public int validate(String input) {
         int attemptCount = parseToInt(input);
