7a3b9d576259cafe41e016247018b007f39d43d3
softmoca <acyc0909@gmail.com>
2025-11-09 23:18:27 +0900

    feat(MovingStrategy) : 이동 전략 인터페이스 기능 구현

---
+++ src/main/java/racingcar/RandomMovingStrategy.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomMovingStrategy implements MovingStrategy {
+
+    private static final int MIN_RANGE = 0;
+    private static final int MAX_RANGE = 9;
+    private static final int MOVE_THRESHOLD = 4;
+
+    @Override
+    public boolean shouldMove() {
+        int randomValue = Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE);
+
+        return randomValue >= MOVE_THRESHOLD;
+    }
+}
