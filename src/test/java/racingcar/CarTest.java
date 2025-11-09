953c2083e96f2e7f75f4f4774e0d061884d5e859
softmoca <acyc0909@gmail.com>
2025-11-09 23:04:15 +0900

    test(Car) : 5자를 초과하는 이름으로 자동차 생성 테스트 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,15 @@ class CarTest {
 
         // then
         assertThat(car.getName()).isEqualTo("pobi");
+    }
+
+
+    @Test
+    void 이름이_5자를_초과하면_예외가_발생한다() {
+
+        assertThatThrownBy(() -> new Car("pobi12"))
+                .isInstanceOf(IllegalArgumentException.class);
+
     }
 
 
