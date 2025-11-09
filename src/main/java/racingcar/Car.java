cbc520f7c033fd6ce14554b6e31e3b14756a080c
softmoca <acyc0909@gmail.com>
2025-11-09 23:05:11 +0900

    feat(Car) : 5자 이하의 이름 검증 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,6 +6,9 @@ public class Car {
     private String name;
 
     public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
         this.name = name;
     }
 
