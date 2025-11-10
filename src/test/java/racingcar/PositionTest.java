4d73b89d51cb81252f197914ead418e7d81d5658
softmoca <acyc0909@gmail.com>
2025-11-10 11:17:51 +0900

    test(Position) : 초기 위치 및 검증 테스트 추가

---
+++ src/test/java/racingcar/PositionTest.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+
+    @Test
+    void 초기_위치_0으로_생성된다() {
+        Position position = Position.start();
+
+        assertThat(position.getValue()).isEqualTo(0);
+    }
+
+    @Test
+    void 양수_위치로_생성할_수_있다() {
+        Position position = Position.from(3);
+
+        assertThat(position.getValue()).isEqualTo(3);
+    }
+
+    @Test
+    void 음수_위치는_생성할_수_없다() {
+        assertThatThrownBy(() -> Position.from(-1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
