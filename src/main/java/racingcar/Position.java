fed2e785da000bca9be0003df811568a5d0e9e3b
softmoca <acyc0909@gmail.com>
2025-11-10 11:22:27 +0900

    feat(Position) : equals 기능 구현

--- src/main/java/racingcar/Position.java
+++ src/main/java/racingcar/Position.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 
+import java.util.Objects;
+
 public class Position {
 
     private final int value;
@@ -33,5 +35,17 @@ public class Position {
         return value;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return value == position.value;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
 }
