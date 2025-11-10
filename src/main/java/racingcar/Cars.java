34c8d47b34c053b2e95e0a928d367e945c671d01
softmoca <acyc0909@gmail.com>
2025-11-10 11:50:39 +0900

    feat(Cars) : 우승자 찾기 기능 구현

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -43,5 +43,17 @@ public class Cars {
                 .collect(Collectors.toList());
     }
 
+    public List<String> getWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+
 
 }
