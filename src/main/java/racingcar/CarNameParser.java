496ea1c644f9678ca84306f3a87091e45c567505
softmoca <acyc0909@gmail.com>
2025-11-10 12:33:10 +0900

    feat(CarNameParser) : 빈문자열 입력시 예외 처리 기능 구현

--- src/main/java/racingcar/CarNameParser.java
+++ src/main/java/racingcar/CarNameParser.java
@@ -9,6 +9,7 @@ public class CarNameParser {
     private static final String DELIMITER = ",";
 
     public static List<String> parse(String input) {
+        validateInput(input);
 
         List<String> names = Arrays.stream(input.split(DELIMITER))
                 .map(String::trim)
@@ -17,5 +18,10 @@ public class CarNameParser {
         return names;
     }
 
+    private static void validateInput(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+        }
+    }
 
 }
