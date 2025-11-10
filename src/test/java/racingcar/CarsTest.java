248586105b98b3de4d3f173bb48e209ef682b280
softmoca <acyc0909@gmail.com>
2025-11-10 11:38:11 +0900

    test(Cars) : 생성 테스트 추가

---
+++ src/test/java/racingcar/CarsTest.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class CarsTest {
+
+    @Test
+    void 이름_목록으로_여러_대의_자동차를_생성한다() {
+        // given
+        List<String> names = Arrays.asList("pobi", "woni", "jun");
+
+        // when
+        Cars cars = Cars.from(names);
+
+        // then
+        assertThat(cars.size()).isEqualTo(3);
+    }
+}
