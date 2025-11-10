3ddba41a3fa2e0976ff5fe31b8c5c0484ef23e3c
softmoca <acyc0909@gmail.com>
2025-11-10 12:34:44 +0900

    feat(CarNameParser) : 빈이름이 있는 자동차 예외처리 기능 구현

--- src/main/java/racingcar/CarNameParser.java
+++ src/main/java/racingcar/CarNameParser.java
@@ -14,13 +14,19 @@ public class CarNameParser {
         List<String> names = Arrays.stream(input.split(DELIMITER))
                 .map(String::trim)
                 .collect(Collectors.toList());
-
+        validateNames(names);
         return names;
     }
 
     private static void validateInput(String input) {
         if (input == null || input.trim().isEmpty()) {
             throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+        }
+    }
+
+    private static void validateNames(List<String> names) {
+        if (names.stream().anyMatch(String::isEmpty)) {
+            throw new IllegalArgumentException("빈 이름은 입력할 수 없습니다.");
         }
     }
 
