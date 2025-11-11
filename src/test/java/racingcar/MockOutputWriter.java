0c19855ca864fad93ffe7bbac656b074792ebaec
softmoca <acyc0909@gmail.com>
2025-11-11 22:53:58 +0900

    test(RacingController) : 출력 테스트를 위한 Mock 구현

---
+++ src/test/java/racingcar/MockOutputWriter.java
@@ -0,0 +1,47 @@
+package racingcar;
+
+import java.util.List;
+import racingcar.view.OutputWriter;
+
+class MockOutputWriter implements OutputWriter {
+    private boolean headerPrinted = false;
+    private int roundCount = 0;
+    private boolean winnersPrinted = false;
+    private String errorMessage = null;
+
+    @Override
+    public void printHeader() {
+        headerPrinted = true;
+    }
+
+    @Override
+    public void printRound(List<String> names, List<Integer> positions) {
+        roundCount++;
+    }
+
+    @Override
+    public void printWinners(List<String> winners) {
+        winnersPrinted = true;
+    }
+
+    @Override
+    public void printError(String message) {
+        errorMessage = message;
+    }
+
+    public boolean isHeaderPrinted() {
+        return headerPrinted;
+    }
+
+    public int getRoundCount() {
+        return roundCount;
+    }
+
+    public boolean isWinnersPrinted() {
+        return winnersPrinted;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
