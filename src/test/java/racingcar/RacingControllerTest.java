8deff10dd3ea8e1fa1234c85051b8b6a76b63537
softmoca <acyc0909@gmail.com>
2025-11-11 23:05:44 +0900

    test(RacingController) : 잘못된 입력  테스트 추가

--- src/test/java/racingcar/RacingControllerTest.java
+++ src/test/java/racingcar/RacingControllerTest.java
@@ -3,6 +3,7 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import racingcar.controller.RacingController;
 import racingcar.view.InputReader;
@@ -30,6 +31,34 @@ class RacingControllerTest {
         assertThat(outputWriter.isHeaderPrinted()).isTrue();
         assertThat(outputWriter.getRoundCount()).isEqualTo(3);
         assertThat(outputWriter.isWinnersPrinted()).isTrue();
+    }
+
+    @Test
+    void 잘못된_입력시_에러를_출력한다() {
+        // given
+        InputReader inputReader = new InputReader() {
+            @Override
+            public List<String> readCarNames() {
+                throw new IllegalArgumentException("잘못된 입력");
+            }
+
+            @Override
+            public int readRounds() {
+                return 0;
+            }
+        };
+
+        MockOutputWriter outputWriter = new MockOutputWriter();
+        RacingController controller = new RacingController(
+                inputReader,
+                outputWriter
+        );
+
+        // when
+        controller.run();
+
+        // then
+        assertThat(outputWriter.getErrorMessage()).isEqualTo("잘못된 입력");
     }
 
 }
